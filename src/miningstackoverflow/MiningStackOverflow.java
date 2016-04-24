2b1cc00fae8c741bbc0ac0a495a0ad48194a564e
mr5ba <mr5ba@ELM.cs.virginia.edu>
2016-04-23 22:19:37 -0400

    tag list and multiple keyword search in title. Return post with title and creationdate

--- src/miningstackoverflow/MiningStackOverflow.java
+++ src/miningstackoverflow/MiningStackOverflow.java
@@ -14,10 +14,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
@@ -34,9 +37,9 @@ import java.util.logging.Logger;
 public class MiningStackOverflow {
     public static String pathxml =  "C:/Development/NetbeanProjects/data/miningstackoverflow/Posts.xml";
    // public static String pathxml =  "C:/Development/NetbeanProjects/data/miningstackoverflow/Posts_sample.xml";
-    public static String writepath = "C:/Development/NetbeanProjects/data/miningstackoverflow/Android_Posts_title.txt";
+    public static String writepath = "C:/Development/NetbeanProjects/data/miningstackoverflow/Android_Posts_title_tag_date.txt";
     public static String rootpath = "C:/Development/NetbeanProjects/data/miningstackoverflow/";
-    public static String readpath = "C:/Development/NetbeanProjects/data/miningstackoverflow/Android_Posts_title.txt";
+    public static String readpath = "C:/Development/NetbeanProjects/data/miningstackoverflow/Android_Posts_title_tag_date.txt";
     
     /**
      * @param args the command line arguments
@@ -63,27 +66,82 @@ public static void main(String[] args) {
      HashMap<String, ArrayList<String>> title_tags;
     // TODO code application logic here
     MiningStackOverflow mso = new MiningStackOverflow();
-    title_tags = mso.loadTitles(readpath);
+    ArrayList<String> tag_key = new ArrayList<>();
+    tag_key.add("android-5.0-lollipop");
+    tag_key.add("android-5.1.1-lollipop");
+    String fileName = "android-5.1.1-lollipop";
+    ArrayList<String> searchTest = new ArrayList<>();
+        searchTest.add("android 5");
+        searchTest.add("android 5.0");
+        searchTest.add("android-5");
+        searchTest.add("android-5.0");
+        searchTest.add("android lollipop");
+        searchTest.add("android-lollipop");
+        searchTest.add("android-5.0-lollipop");
+        searchTest.add("Lollipop");
+        /*searchTest.add("android 5");
+        searchTest.add("android 5.0");
+        searchTest.add("android-5");
+        searchTest.add("android-5.0");
+        searchTest.add("android lollipop");
+        searchTest.add("android-lollipop");
+        searchTest.add("android-5.0-lollipop");
+        searchTest.add("Lollipop");*/
+    HashMap<Integer, Integer> tag_timeline = mso.loadTitlesDate(readpath, tag_key, searchTest);
+    mso.writeMapOnFileInteger(tag_timeline,fileName+".txt");
+    
+   // mso.checkDate();
+//    title_tags = mso.loadTitles(readpath);
     //System.out.println("Title map size: "+title_tags.size());
     ArrayList<String> baseType1 = new ArrayList<>();
-    baseType1.add("android-maps");
-    baseType1.add("android-emulator");
-    baseType1.add("android-layout");
-    baseType1.add("android-studio");
-    baseType1.add("android-debugging");
+    baseType1.add("android-4.4-kitkat");
+ //   baseType1.add("android-4.4");
+ //   baseType1.add("kitkat");
+   // baseType1.add("android-studio");
+    //baseType1.add("android-debugging");
 
     double param = 0.5;
    
-    mso.rankTitle(title_tags, baseType1, param);
+//    mso.rankTitle(title_tags, baseType1, param);
     //mso.writePosts(post_set, "Title", "Android_Posts_title.txt");
-     //post_set = mso.getPosts(pathxml,writepath);
+//     post_set = mso.getPosts(pathxml,writepath);
+}
+private boolean checkDate(){
+    SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd"); 
+
+      String input ="2012-10-31T16:42:47.213"; 
+        String input2 ="2015-07-31T21:42:52.667"; 
+
+      System.out.print(input + " Parses as "); 
+
+      Date t; 
+        Date t2; 
+
+      try { 
+          t = ft.parse(input); 
+          t2 = ft.parse(input2);
+          System.out.println(t);
+          int flag = t.compareTo(t2);
+          if(flag<0){
+              System.out.println(t+ " t is older than "+t2);
+          }else if(flag ==0){
+               System.out.println("They are equal");
+          }else{
+              System.out.println(t2+" t2 is older than "+t);
+          }
+      } catch (ParseException e) { 
+          System.out.println("Unparseable using " + ft); 
+      }
+      return true;
 }
 private void rankTitle(HashMap<String, ArrayList<String>> titleMap, ArrayList<String> baseTagList,double wparam){
     HashMap<String, Double> ranks = new HashMap<>();
     for(String title:titleMap.keySet()){
         ArrayList<String> postTagList = titleMap.get(title);
         double score = calculateScore(postTagList, baseTagList, wparam);
-        ranks.put(title, score);
+        if(score>0){
+            ranks.put(title, score);
+        }
     }
     ranks = sortByComparatorDouble(ranks, DESC);
     writeOnFile_doubleHash(ranks,"ranks_title.txt");
@@ -105,6 +163,73 @@ private double calculateScore(ArrayList<String> postTagList, ArrayList<String> b
     score =score/(baseTagList.size()+1);//normalized over base length
     //System.out.println("Post:"+postTagList.toString()+"\n \t"+baseTagList.toString()+"\n \t"+common.toString());
     return score;//[0 to 1]
+}
+private HashMap<Integer, Integer>loadTitlesDate(String filepath, ArrayList<String> tTag,ArrayList<String> searchTest){
+    HashMap<Integer, Integer> ym_date_map = new HashMap<>();
+   BufferedReader reader=null;
+    try {
+        reader = new BufferedReader(new InputStreamReader(new FileInputStream(filepath), "UTF-8"));
+        String title ;
+        String tags;
+        String cdate;
+        while ((title = reader.readLine()) != null && (tags=reader.readLine()) !=null && (cdate=reader.readLine()) !=null) {
+               
+                    tags = tags.replaceAll("\\[", "");
+                    tags = tags.replaceAll("]", "");
+                    String[] tags_parts = tags.trim().split(",");
+                    ArrayList<String> tagList = new ArrayList<>();
+                    for (String tags_part : tags_parts) {
+                        tagList.add(tags_part.trim());
+                    }
+                    boolean inTitle=false;
+                    for(String substr:searchTest){
+                        if(title.toLowerCase().contains(substr.toLowerCase())){
+                            inTitle =true;
+                            break;
+                        }
+                    }
+                    boolean inTags = false;
+                    for(String tag:tTag){
+                        if(tagList.contains(tag)){
+                            inTags=true;
+                            break;
+                        }
+                    }
+                    if(inTags||inTitle){//if in title or in tags
+                        String ym="";
+                        String[] date_parts = cdate.trim().split("-");
+                        if(date_parts.length>2){
+                            ym = date_parts[0].trim()+date_parts[1].trim();
+                        }
+                        Integer ym_date = Integer.parseInt(ym);
+                        Integer freq = ym_date_map.get(ym_date);
+                        if(freq==null){
+                            ym_date_map.put(ym_date, 1);
+                        }else{
+                            freq++;
+                            ym_date_map.put(ym_date, freq);
+                        }
+                    }
+                    //titletags.put(title, tagList);
+                   // System.out.println("Title: "+title+"="+tagList.toString());
+              
+           
+        }
+        
+    }catch(Exception e){
+        e.printStackTrace();
+    }
+   finally{
+        if(reader !=null){
+            try {
+                reader.close();
+            } catch (IOException ex) {
+                Logger.getLogger(MiningStackOverflow.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+   }
+    return ym_date_map;
 }
 private HashMap<String, ArrayList<String>>loadTitles(String filepath){
     HashMap<String, ArrayList<String>> titletags = new HashMap<>();
@@ -159,7 +284,7 @@ public void writeOnFile_doubleHash(HashMap<String,Double>wmap,String fileName)
     for (String name: wmap.keySet()){
        String key =name;
        String value = wmap.get(name).toString();
-       bw.write(key+" "+value+"\n");
+       bw.write(key+"\t"+value+"\n");
     }
     System.out.println("Write Successfull: "+fileName);
 
@@ -311,16 +436,16 @@ private HashSet<Post> getPosts(String readfilepath, String writepath){
                     String CommunityOwnedDate = CommunityOwnedDate_parts[1].trim().split("\"")[1].trim();
                     p.setCommunityOwnedDate(CommunityOwnedDate);
                }
-               
-            }
-            }
-            countpost++;
-            if(countpost%10000==0){
+                countpost++;
+                if(countpost%10000==0){
                 System.out.println("Written: "+countpost + " Skipped: "+skipped_count);
-             }
+                }
             //postSet.add(p);
-              bw.write(p.getTitle()+"\n"+p.getTags().toString()+"\n");      
-            // System.out.println(p); 
+                 bw.write(p.getTitle()+"\n"+p.getTags().toString()+"\n"+p.getCreationDate()+"\n");      
+            // System.out.println(p);   
+            }
+            }
+            
             
        }
     }catch(Exception e){
@@ -429,6 +554,38 @@ private HashSet<Post> getPosts(String readfilepath, String writepath){
     tags_map = sortByComparator(tags_map, DESC);
     writeMapOnFile(tags_map, "tags_java.txt");
   }
+    public void writeMapOnFileInteger(HashMap<Integer,Integer>wmap,String fileName)
+{
+   BufferedWriter bw = null;
+   try {
+    File file = new File(fileName);
+     if (!file.exists()) {
+        file.createNewFile();
+     }
+     FileWriter fw = new FileWriter(rootpath+file);
+     bw = new BufferedWriter(fw);
+    for (Integer key: wmap.keySet()){
+     
+       Integer value = wmap.get(key);
+       bw.write(key+"\t"+value+"\n");
+    }
+    System.out.println("Write Successfull: "+fileName);
+
+ } catch (IOException ioe) {
+      ioe.printStackTrace();
+   }
+   finally
+   { 
+      try{
+         if(bw!=null)
+            bw.close();
+      }catch(Exception ex){
+          System.out.println("Error in closing the BufferedWriter"+ex);
+       }
+
+   }
+
+}
 public void writeMapOnFile(HashMap<String,Integer>wmap,String fileName)
 {
    BufferedWriter bw = null;
@@ -442,7 +599,7 @@ public void writeMapOnFile(HashMap<String,Integer>wmap,String fileName)
     for (String name: wmap.keySet()){
        String key =name;
        String value = wmap.get(name).toString();
-       bw.write(key+" "+value+"\n");
+       bw.write(key+"\t"+value+"\n");
     }
     System.out.println("Write Successfull: "+fileName);
 
